0857beae56510705550d300aaf1ab52286e48ceb
Victor-Avilla <victorfonsecaavila@gmail.com>
2025-09-19 20:47:59 -0300

    feat: componente de dados de mercado

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 
 import '../styles/index.css'
 import Header from './Header'
+import Dashboard from './Dashboard'
 
 function App() {
     return(
         <div className='bg-slate-950 w-screen h-screen'>
             
-            <div><Header /></div>
+            <Header />
+            <Dashboard />
 
         </div>
     )
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,11 @@
+function Dashboard(){
+    return(
+        <section className='grid grid-cols-3 md:grid-cols-2 lg:grid-cols-1 w-3xs gap-6 h-36 bg-slate-900 rounded-md border border-slate-800'>
+
+
+
+        </section>
+    )
+}
+
+export default Dashboard
