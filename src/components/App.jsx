d5b4ac5a1ba64edbe19d8d6d244eb8a0f7f0c556
Victor-Avilla <victorfonsecaavila@gmail.com>
2025-09-29 23:27:29 -0300

    feat: criado dados placeholders para os props do componente cards

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,9 +5,11 @@ import Header from './Header'
 import Dashboard from './Dashboard'
 import Cards from './Cards'
 
+import { TrendingUp, TrendingDown,PiggyBank, ChartNoAxesColumn, Percent, Wallet } from 'lucide-react'
+
 function App() {
     return (
-        <div className='bg-slate-950 w-full h-screen'>
+        <div className='bg-slate-950 h-screen w-screen '>
 
             <Header />
 
@@ -17,7 +19,13 @@ function App() {
             lg:grid-cols-6 lg:mx-30
             xl:mx-40 xl'>
 
-                
+                <Cards Icon={PiggyBank} iconColor={'bg-sky-800'} bgIconColor={'text-sky-200'} title={'Patrimonio Total'} data={'R$ 100.000,00'} value={'+5.3%'} />
+                <Cards Icon={Wallet} iconColor={'bg-violet-800'} bgIconColor={'text-violet-200'} title={'Ativos'} data={'5'} value={'4 categorias'} />
+                <Cards Icon={ChartNoAxesColumn} iconColor={'bg-green-700'} bgIconColor={'text-green-200'} title={'Rendimento'} data={'-R$ 2.000,00 '} value={'-0.2% mês'} />
+                <Cards Icon={TrendingUp} iconColor={'bg-emerald-800'} bgIconColor={'text-emerald-200'} title={'Maior Alta'} data={'BTC'} value={'+10.6%'} />
+                <Cards Icon={Percent} iconColor={'bg-yellow-800'} bgIconColor={'text-yellow-200'} title={'Performance'} data={'97.4%'} value={'vs IBOV'} />
+                <Cards Icon={TrendingDown} iconColor={'bg-red-800'} bgIconColor={'text-red-200'} title={'Maior Baixa'} data={'VALE3'} value={'-3.4%'} />
+
 
             </div>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,6 @@
 function Dashboard(){
     return(
-        <section className=' max-w-full m-5 md:mx-20 xl:mx-40'>
+        <section className=' max-w-full m-5 md:mx-10 xl:mx-5'>
 
         <div className="max-w-full h-96 bg-slate-900 rounded-md border border-slate-800">1</div>
 
